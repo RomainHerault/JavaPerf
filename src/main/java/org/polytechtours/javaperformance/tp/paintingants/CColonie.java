dd16407b154e86d1ed3e4d1713b21359b74b3059
RomainHerault <heraultromain8@gmail.com>
2019-12-10 09:49:52 +0100

    Creation

--- src/main/java/org/polytechtours/javaperformance/tp/paintingants/CColonie.java
+++ src/main/java/org/polytechtours/javaperformance/tp/paintingants/CColonie.java
@@ -3,7 +3,7 @@ package org.polytechtours.javaperformance.tp.paintingants;
 /*
  * CColonie.java
  *
- * Created on 11 avril 2007, 16:35
+ * Created on 11 avril 2007, 16:36
  *
  * To change this template, choose Tools | Options and locate the template under
  * the Source Creation and Management node. Right-click the template and choose
@@ -13,36 +13,44 @@ import java.util.Vector;
 
 public class CColonie implements Runnable {
 
-  private Boolean mContinue = Boolean.TRUE;
-  private Vector<CFourmi> mColonie;
-  private PaintingAnts mApplis;
+	private Boolean mContinue = Boolean.TRUE;
+	private Vector<CFourmi> mColonie;
+	private PaintingAnts mApplis;
 
-  /** Creates a new instance of CColonie */
-  public CColonie(Vector<CFourmi> pColonie, PaintingAnts pApplis) {
-    mColonie = pColonie;
-    mApplis = pApplis;
-  }
+	/** Creates a new instance of CColonie */
+	public CColonie(Vector<CFourmi> pColonie, PaintingAnts pApplis)
+	{
+		mColonie = pColonie;
+		mApplis = pApplis;
+	}
 
-  public void pleaseStop() {
-    mContinue = false;
-  }
+	public void pleaseStop()
+	{
+		mContinue = false;
+	}
 
-  @Override
-  public void run() {
+	@Override
+	public void run()
+	{
 
-    while (mContinue == true) {
-      if (!mApplis.getPause()) {
-        for (int i = 0; i < mColonie.size(); i++) {
-          mColonie.get(i).deplacer();
-          mApplis.compteur();
-        }
-      } else {
-        /*
-         * try { Thread.sleep(100); } catch (InterruptedException e) { break; }
-         */
+		while(mContinue == true)
+		{
+			if(!mApplis.getPause())
+			{
+				for(int i = 0; i < mColonie.size(); i++)
+				{
+					mColonie.get(i).deplacer();
+					mApplis.compteur();
+				}
+			}
+			else
+			{
+				/*
+				 * try { Thread.sleep(100); } catch (InterruptedException e) { break; }
+				 */
 
-      }
-    }
-  }
+			}
+		}
+	}
 
 }
